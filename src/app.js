88d76e2cb022ea7dd035a311f4a7ea2df207d786
almost-annonymous <anam.dhala@gmail.com>
2025-04-01 21:16:21 +0000

    first commit for js excuse generator project

--- src/app.js
+++ src/app.js
@@ -7,5 +7,20 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let who = ['A Florida Man', 'My evil twin', 'An army of angry goldfish', 'A group of grumpy giraffes', 'The police'];
+  let action = ['stole', 'set fire to', 'accidentally sacrificed', 'lost their minds to', 'soaked in water'];
+  let when = ['while I was sleeping', 'in the middle of broad daylight', 'when dinosaurs roamed the Earth', 'while I used the bathroom','before the aliens took over'];
+  let what = ['my phone', 'my dignity', 'my legs', 'my project', 'my computer'];
+
+  function getRandomIndex (a, b) {
+    return Math.floor(Math.random() * (a - b) + b);
+  }
+  function getRandomExcuse (anyArray) {
+    let a = 0;
+    let b = 4;
+    let random = getRandomIndex (a, b);
+    return anyArray[random];
+  }
+  document.querySelector("#excuse").innerHTML = getRandomExcuse(who) + ' ' + getRandomExcuse(action) + ' ' + getRandomExcuse(what) + ' ' + getRandomExcuse(when) + '!';
+
 };
