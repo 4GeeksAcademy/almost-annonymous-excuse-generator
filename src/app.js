592d135ccf93ac8c091586f9e3cf7d382865aec1
almost-annonymous <anam.dhala@gmail.com>
2025-04-01 21:40:36 +0000

    second commit - added changing background colors and cleaned code

--- src/app.js
+++ src/app.js
@@ -11,16 +11,17 @@ window.onload = function() {
   let action = ['stole', 'set fire to', 'accidentally sacrificed', 'lost their minds to', 'soaked in water'];
   let when = ['while I was sleeping', 'in the middle of broad daylight', 'when dinosaurs roamed the Earth', 'while I used the bathroom','before the aliens took over'];
   let what = ['my phone', 'my dignity', 'my legs', 'my project', 'my computer'];
+  let background = ["rgb(248, 11, 11)", "rgb(6, 28, 224)", "rgb(0, 247, 0)", "rgb(208, 255, 0)", "rgb(148, 64, 226)"]
 
   function getRandomIndex (a, b) {
-    return Math.floor(Math.random() * (a - b) + b);
+    return Math.floor(Math.random() * (b - a + 1));
   }
   function getRandomExcuse (anyArray) {
-    let a = 0;
-    let b = 4;
-    let random = getRandomIndex (a, b);
-    return anyArray[random];
+    return anyArray[getRandomIndex(0, anyArray.length - 1)];
+  }
+  function getRandomColor (background) {
+    return background[getRandomIndex(0, background.length - 1)];
   }
   document.querySelector("#excuse").innerHTML = getRandomExcuse(who) + ' ' + getRandomExcuse(action) + ' ' + getRandomExcuse(what) + ' ' + getRandomExcuse(when) + '!';
-
+  document.body.style.backgroundColor = getRandomColor(background);
 };
